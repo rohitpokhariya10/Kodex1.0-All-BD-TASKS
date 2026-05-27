bf87dcef57b58844b683ca021f415743c1c13139
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-27 17:55:53 +0530

    Configure database connection

--- Backend-CRUD-notes-app/CRUD-1/server.js
+++ Backend-CRUD-notes-app/CRUD-1/server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const app = require("./src/app");
+const connectToDb = require("./src/config/db");
 
+
+
+connectToDb();
 
 let port = 3000 || 8000;
 app.listen(port , ()=>{
