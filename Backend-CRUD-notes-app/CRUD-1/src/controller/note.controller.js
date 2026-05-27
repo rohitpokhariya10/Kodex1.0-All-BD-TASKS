a1d1aeafbfb45d858fd93de9579ac54d9f78d258
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-27 18:35:21 +0530

    Create notes route

--- Backend-CRUD-notes-app/CRUD-1/src/controller/note.controller.js
+++ Backend-CRUD-notes-app/CRUD-1/src/controller/note.controller.js
@@ -1,5 +1,16 @@
+const noteModel = require("../models/notes.model");
+const { createNoteService } = require("../service/note.services");
+
 const createNoteController = async (req, res) => {
-    let {titl}
+let newNote = await  createNoteService(req.body);
+ 
+ 
+  console.log(newNote);
+
+  return res.status(201).json({
+    message:"Note Created",
+    newNote
+  })
 };
 
 module.exports = { createNoteController };
