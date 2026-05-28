32435c93211051ad2632648a63b608992925ff3e
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-28 16:39:44 +0530

    user model created for authentication

--- BD-CRUD-with-authentication/src/app.js
+++ BD-CRUD-with-authentication/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const errorMiddleware = require("./middleware/error.middleware");
 
 const app = express();
 
 app.use(express.json());
 
+app.use(errorMiddleware);
 module.exports = app;
