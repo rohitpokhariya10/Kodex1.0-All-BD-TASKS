6e5f8ba5e7ffdc3c1eeadccf98836e1b9ae5f01a
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-29 16:02:17 +0530

    Create a authRouter for creating authentication routes and use in app.js file

--- BD-ecommerce-product-api/src/app.js
+++ BD-ecommerce-product-api/src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./utils/apiError");
+const authRouter = require("./routes/auth.routes");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+
+app.use("/api/auth" , authRouter);
 
 app.use(errorMiddleware);
 
