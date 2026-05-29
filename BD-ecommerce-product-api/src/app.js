5eb08774daf6cfe1813f3902e2693bdb4a82a6e4
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-29 16:00:11 +0530

    Use global error handler middleware in app.js

--- BD-ecommerce-product-api/src/app.js
+++ BD-ecommerce-product-api/src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./utils/apiError");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-
+app.use(errorMiddleware);
 
 module.exports = app;
