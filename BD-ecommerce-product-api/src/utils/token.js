54285e137c0fd61d918689bfacc07d31f56232ec
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-29 16:07:26 +0530

    Create helper function to generate AccessToken and RefreshToken

---
+++ BD-ecommerce-product-api/src/utils/token.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const generateAccessToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+  });
+};
+
+const generateRefreshToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+  });
+};
+
+module.exports = {generateAccessToken , generateRefreshToken}
